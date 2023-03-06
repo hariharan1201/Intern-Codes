b37b39ca987fad9a11a62cba1a40cabfaf21f52c
HariharanM <hariakash1117@gmail.com>
2023-03-06 17:00:08 +0530

    Declaration of array

--- list.dart
+++ list.dart
@@ -1,6 +1,8 @@
 main() {
-  var lis = [10, 20];
+  var lis = [10, 20, 30, 40, 50, "Hello"];
   for (int i = 0; i < lis.length; i++) {
     print(lis[i]);
   }
+  List arr = [10, 30, 40];
+  print(arr);
 }
