662bc37181616a288c11f07858f8306930a7232e
HariharanM <hariakash1117@gmail.com>
2023-03-06 16:38:55 +0530

    Function and comments

--- func.dart
+++ func.dart
@@ -2,6 +2,8 @@ main() {
   print(add("hello"));
 }
 
+//dynamic is data type that can handle any data value
 dynamic add(dynamic value) {
   return value;
+  //Return any data type because of dynamic type
 }
