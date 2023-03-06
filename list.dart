0021f3a12dc302f290cfdda0103007e9f6cdb33f
HariharanM <hariakash1117@gmail.com>
2023-03-06 17:07:04 +0530

    Array handling

--- list.dart
+++ list.dart
@@ -1,7 +1,9 @@
 main() {
-  var lis = [10, 20, 30, 40, 50, "Hello"];
+  var lis = [10, "Hello", 2.5, 'c'];
   for (int i = 0; i < lis.length; i++) {
     print(lis[i]);
+    print((lis[i]).runtimeType);
+    print("-----------");
   }
   List arr = [10, 30, 40];
   print(arr);
