58de4e2d8c81f79b3d7e6b5756a46d785bd50e93
HariharanM <hariakash1117@gmail.com>
2023-03-07 11:40:28 +0530

    Operators

---
+++ Taskset 1/operators.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+//Operators of Dart
+main() {
+  int a = 10;
+  int b = 5;
+  var nope = stdin.readLineSync();
+  var c = "hello ite me";
+  print(a / b);
+  print(a ~/ b);
+  print(b.runtimeType);
+  print(c.runtimeType);
+  print(nope);
+  print(a > b ? "AAAAAAAA" : "Bbbbbbbb"); //Here complete the operators of dart
+}
